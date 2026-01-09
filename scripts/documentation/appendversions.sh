f5e42616ea85df2be4f28552bdecb750ad12bdea
Guangzhen Jin <158495465+Guangzhen-Jin@users.noreply.github.com>
2026-01-09 08:58:14 -0500

    Refactor lead and tail regex patterns in script
    Updated regex patterns for lead and tail variables to handle whitespace.

--- scripts/documentation/appendversions.sh
+++ scripts/documentation/appendversions.sh
@@ -16,32 +16,12 @@ inventory_json="$repo_path/scripts/documentation/rcac_biocontainers_inventory.js
 
 for containername in $containernamesarray
 do
-    # inputfolder="$repo_path/module_files/$containername/"
-    # echo "input folder: "$inputfolder
-
-    # outputfile="$sourcefolder$containername/$containername.rst"
-    # echo "~~~~~~~~" > tempfile.rst
-    # filenamesarray=`ls $inputfolder*.lua`
-
-    # for eachfile in $filenamesarray
-    # do
-    #     echo -n "- " >> tempfile.rst
-    #     eachfile=${eachfile::-4}
-    #     echo "$eachfile" | sed 's:.*/::' >> tempfile.rst
-    # done
-    # echo "" >> tempfile.rst
-
-    # lead='^Versions$'
-    # tail='^Commands$'
-    # output=$(sed -e "/$lead/,/$tail/{ /$lead/{p; r tempfile.rst
-    #     }; /$tail/p; d }"  $outputfile)
-    # echo "$output" > $outputfile
     outputfile="$sourcefolder$containername/$containername.rst"
     jq -e --arg app "$containername" '.[$app]' "$inventory_json" > /dev/null || continue
     echo "Updating versions for $containername"
     # Build Versions table
     {
-         echo "~~~~~~~"
+        echo "~~~~~~~"
         echo ".. list-table::"
         echo "  :width: 100 %"
         echo "  :widths: 25 75"
@@ -62,8 +42,10 @@ do
     } > tempfile.rst
 
 
-    lead='^Versions$'
-    tail='^Commands$'
+    # lead='^Versions$'
+    # tail='^Commands$'
+    lead='^[[:space:]]*Versions[[:space:]]*$'
+    tail='^[[:space:]]*Commands[[:space:]]*$'
 
     output=$(sed -e "/$lead/,/$tail/{ /$lead/{p; r tempfile.rst
         }; /$tail/p; d }" "$outputfile")
